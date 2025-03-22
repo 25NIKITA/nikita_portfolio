be9877c898d77a72c5d8e456b368a87a682e87ae
Nikita <113427756+25NIKITA@users.noreply.github.com>
2025-03-22 11:42:00 +0530

    Fix Vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/nikita_portfolio/", // Replace with your repository name
   plugins: [react()],
+  base: "./", // Use './' or '/your-repo-name/' if deployed on a subpath
 });
